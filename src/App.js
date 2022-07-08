fc351fe42bdeb5609df55944e949a85feac33bb9
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-07-08 20:53:19 +0100

    Edit package.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 const App = () => <h1>Hallo React</h1>;
